e4cd162fa516b142dd6a279c54eb4b6b9b115367
Jasper <fenench@gmail.com>
2024-07-26 06:20:31 +0100

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,6 +17,9 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
+	if(args.length()<2){
+		return 0;
+}
     String language = args[0];
     String country = args[1];
     String name = args[2];
